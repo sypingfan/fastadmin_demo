b772712c23846031453ea08fe62a2607cf5b5faa
刘平凡 <liu2476979217@163.com>
2024-05-12 22:56:44 +0800

    FastAdmin的安装及自动创建CURD

---
+++ application/admin/lang/zh-cn/pim/schedule.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'Stime'       => '开始时间',
+    'Etime'       => '结束时间',
+    'Title'       => '事务',
+    'Maincontent' => '内容说明',
+    'Attachfile'  => '附件',
+    'Status'      => '状态',
+    'Status 0'    => '未开始',
+    'Set status to 0'=> '设为未开始',
+    'Status 1'    => '进行中',
+    'Set status to 1'=> '设为进行中',
+    'Status 2'    => '已完成',
+    'Set status to 2'=> '设为已完成',
+    'Status 9'    => '已取消',
+    'Set status to 9'=> '设为已取消',
+    'Createtime'  => '创建时间',
+    'Updatetime'  => '更新时间',
+    'Deletetime'  => '删除时间',
+    'Weign'       => '权重'
+];
---
+++ public/assets/js/backend/pim/schedule.js
@@ -0,0 +1,121 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'pim/schedule/index' + location.search,
+                    add_url: 'pim/schedule/add',
+                    edit_url: 'pim/schedule/edit',
+                    del_url: 'pim/schedule/del',
+                    multi_url: 'pim/schedule/multi',
+                    import_url: 'pim/schedule/import',
+                    table: 'pim_schedule',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                fixedColumns: true,
+                fixedRightNumber: 1,
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'stime', title: __('Stime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'etime', title: __('Etime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'title', title: __('Title'), operate: 'LIKE'},
+                        {field: 'attachfile', title: __('Attachfile'), operate: false, formatter: Table.api.formatter.file},
+                        {field: 'status', title: __('Status'), searchList: {"0":__('Status 0'),"1":__('Status 1'),"2":__('Status 2'),"9":__('Status 9')}, formatter: Table.api.formatter.status},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'weign', title: __('Weign')},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        recyclebin: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    'dragsort_url': ''
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: 'pim/schedule/recyclebin' + location.search,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'title', title: __('Title'), align: 'left'},
+                        {
+                            field: 'deletetime',
+                            title: __('Deletetime'),
+                            operate: 'RANGE',
+                            addclass: 'datetimerange',
+                            formatter: Table.api.formatter.datetime
+                        },
+                        {
+                            field: 'operate',
+                            width: '140px',
+                            title: __('Operate'),
+                            table: table,
+                            events: Table.api.events.operate,
+                            buttons: [
+                                {
+                                    name: 'Restore',
+                                    text: __('Restore'),
+                                    classname: 'btn btn-xs btn-info btn-ajax btn-restoreit',
+                                    icon: 'fa fa-rotate-left',
+                                    url: 'pim/schedule/restore',
+                                    refresh: true
+                                },
+                                {
+                                    name: 'Destroy',
+                                    text: __('Destroy'),
+                                    classname: 'btn btn-xs btn-danger btn-ajax btn-destroyit',
+                                    icon: 'fa fa-times',
+                                    url: 'pim/schedule/destroy',
+                                    refresh: true
+                                }
+                            ],
+                            formatter: Table.api.formatter.operate
+                        }
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
